4fcd481a6c1bcc95797432065aca0e7b7264b2df
DumbCaveSpider <56347227+DumbCaveSpider@users.noreply.github.com>
2025-11-17 02:01:00 +0800

    mod logo and badges and other stuff

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 # Rated Layouts
 
-A Geometry Dash mods that introduces rated layouts for levels, allowing players to see the difficulty rating of a level before attempting it.
-Players can earn a new stats called "Blueprint Stars" by completing rated layouts classic levels. This stats is purely for cosmetic purposes and use for tracking in the leaderboards.
+An <cl>Alternative Rating System</c> that focus on rating classic layout levels! It adds a new rating type called <cl>Rated Layouts</c> that rates based on the level's layout design rather than gameplay difficulty.
+This mod intergretes seemlessly with the current rating system and improved <cr>Demon Ratings</c> by providing a more accurate representation of a level's difficulty. <cr>This does not include Platformers levels to be rated</c>.
 
-## Features
+<co>You can view all suggested (Send) layout levels at the Search Tab. You can find the new tabs at the Quick Search Menu.</c>
 
-- Rated Layouts: Levels can now have a difficulty rating (Easy, Normal, Hard, Harder, Insane, Demon) displayed on their layout.
-- Blueprint Stars: Players earn Blueprint Stars for completing rated layouts classic levels. The number of stars earned depends on the difficulty rating of the level.
-- Leaderboard Integration: Players can see their Blueprint Stars on the leaderboards, allowing them to track their progress and compare with others.
-- Rated Layouts Filter: Players can filter levels by their rated layouts in the level browser.
-- Blueprint Difficulty Faces: New difficulty faces for rated layouts levels and has a special Blueprint rings around the difficulty face.
-- Layout Moderators: New role for level moderators who can send layout levels for it to get rated. Levels can also track how many sends it has for rating.
-- Rating System: Similar to Robtop's current rating system, but for rated layouts levels. Levels can be rated by layout moderators based on their design and difficulty.
+## Feature
 
-### All rated levels must go through the developer of this Geode Mod (ArcticWoof) for it to receieve a rating.
+- **Layout Rating System**: Rates levels based on layout design, and fun gameplay. It doesn't focus on the decoration and more on the actual gameplay of the level!
+- **Seamless Integration**: Works alongside the existing rating system, this won't replace the current rating system but rather provide an alternative.
+- **Improved Demon Ratings**: More accurate demon ratings by introducing new difficulty value. <cy>10-30 blueprint stars for rated demon layout levels</cy>!
+- **Faster Rating Process**: Community focus rating system which makes it easier to get levels rated quickly.
+- **Moderator Badges**: Special badges for moderators to identify the layout moderators and admins!
+- **Earn New Blueprint Stars**: Earn blueprint stars by completing rated layout levels!
+- **New Blueprint Creator Points**: Get Blueprint Creator Points whenever you get a rated layout level!
+
+
--- src/server/LevelSearchLayer.cpp
+++ src/server/LevelSearchLayer.cpp
@@ -36,6 +36,13 @@ class $modify(RLLevelSearchLayer, LevelSearchLayer)
                 menu_selector(RLLevelSearchLayer::onRatedLevelsButton));
             quickSearchMenu->addChild(ratedTabButton);
 
+            auto allRatedButtonSprite = SearchButton::create("GJ_longBtn04_001.png", "All Rated", 0.5f, "GJ_sFollowedIcon_001.png");
+            auto allRatedTabButton = CCMenuItemSpriteExtra::create(
+                allRatedButtonSprite,
+                this,
+                menu_selector(RLLevelSearchLayer::onAllRatedLevelsButton));
+            quickSearchMenu->addChild(allRatedTabButton);
+
             // hacky way to use custom icons (inspired by random tab snippet lol)
             auto oldSprite = typeinfo_cast<CCSprite *>(buttonSprite->getChildren()->objectAtIndex(1));
             if (oldSprite)
@@ -54,6 +61,15 @@ class $modify(RLLevelSearchLayer, LevelSearchLayer)
                 newSprite->setPosition(oldSprite2->getPosition());
                 newSprite->setScale(0.8f);
                 ratedButtonSprite->addChild(newSprite);
+            }
+            auto oldSprite3 = typeinfo_cast<CCSprite *>(allRatedButtonSprite->getChildren()->objectAtIndex(1));
+            if (oldSprite3)
+            {
+                auto newSprite = CCSprite::create("rlStarIcon.png"_spr);
+                oldSprite3->setVisible(false);
+                newSprite->setPosition(oldSprite3->getPosition());
+                newSprite->setScale(0.8f);
+                allRatedButtonSprite->addChild(newSprite);
             }
 
             quickSearchMenu->updateLayout();
@@ -119,6 +135,56 @@ class $modify(RLLevelSearchLayer, LevelSearchLayer)
 
         web::WebRequest()
             .param("type", 0)
+            .param("amount", 1000)
+            .get("https://gdrate.arcticwoof.xyz/getLevels")
+            .listen([this](web::WebResponse *res)
+                    {
+                if (res && res->ok()) {
+                    auto jsonResult = res->json();
+                    
+                    if (jsonResult) {
+                        auto json = jsonResult.unwrap();
+                        std::string levelIDs;
+                        bool first = true;
+
+                        if (json.contains("levelIds")) {
+                            auto levelsArr = json["levelIds"];
+                            
+                            // iterate
+                            for (auto levelIDValue : levelsArr) {
+                                auto levelID = levelIDValue.as<int>();
+                                if (levelID) {
+                                    if (!first) levelIDs += ",";
+                                    levelIDs += numToString(levelID.unwrap());
+                                    first = false;
+                                }
+                            }
+                        }
+
+                        if (!levelIDs.empty()) {
+                            auto searchObject = GJSearchObject::create(SearchType::Type19, levelIDs);
+                            auto browserLayer = LevelBrowserLayer::create(searchObject);
+                            auto scene = CCScene::create();
+                            scene->addChild(browserLayer);
+                            auto transitionFade = CCTransitionFade::create(0.5f, scene);
+                            CCDirector::sharedDirector()->pushScene(transitionFade);
+                        } else {
+                            log::warn("No levels found in response");
+                        }
+                    } else {
+                        log::error("Failed to parse response JSON");
+                    }
+                } else {
+                    log::error("Failed to fetch levels from server");
+                } });
+    }
+
+    void onAllRatedLevelsButton(CCObject *sender)
+    {
+        m_fields->m_webTask.cancel();
+
+        web::WebRequest()
+            .param("type", 2)
             .param("amount", 1000)
             .get("https://gdrate.arcticwoof.xyz/getLevels")
             .listen([this](web::WebResponse *res)
